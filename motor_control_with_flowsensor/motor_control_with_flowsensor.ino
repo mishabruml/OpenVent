ea9b0e1b7fef562d557034a969c5605a441808b1
Misha Bruml <mishabruml@gmail.com>
2020-04-17 19:02:51 +0100

    log out info to serial

--- motor_control_with_flowsensor/motor_control_with_flowsensor.ino
+++ motor_control_with_flowsensor/motor_control_with_flowsensor.ino
@@ -10,7 +10,8 @@ int current = 0;     // log filtered current reading
 int a1 = analogRead(A1);      // right POT variable
 int maxPressurePOT = analogRead(A2);      // left POT variable
 int maxPressurePOTconstrianed = 0;
-int freqSettingCounter = 10;              // breaths per min
+int bpm = 10; // breaths per min
+
 const int freqUpButtonPin = 2;
 const int freqDownButtonPin = 6;
 const int redLEDPin = A4;
@@ -72,44 +73,24 @@ void setup()
   md.init();
   Serial.begin(115200);
   Serial.println("Arduino Motor Shield R3");
+  Serial.println("stage\t\tms\t\tcurrent");
   //pinMode(9, OUTPUT);           // brake pin
 
   // set up motor pins
   pinMode(DIR_A, OUTPUT);
   pinMode(PWM_A, OUTPUT);
-
-  // set up UI pins
-  // pinMode(freqUpButtonPin, INPUT);
-  // pinMode(freqDownButtonPin, INPUT);
-  // pinMode(redLEDPin, OUTPUT);
-  // pinMode(blueLEDPin, OUTPUT);
-  // digitalWrite(freqUpButtonPin, HIGH);        // needed for the buttons to work strangely enough
-  // digitalWrite(freqDownButtonPin, HIGH);
-
-  // for (int i = 0; i <= 4; i++)      // flash LEDs a
-  // {
-  //   digitalWrite(redLEDPin, redLEDState);   // set initial LED state
-  //   digitalWrite(blueLEDPin, blueLEDState);   // set initial LED state
-  //   delay(200);
-  //   redLEDState = !redLEDState;
-  //   blueLEDState = !blueLEDState;
-  // }
-  // digitalWrite(redLEDPin, redLEDState);     // turn back off
-  // digitalWrite(blueLEDPin, blueLEDState);   // turn back off
 }
 
 
 void loop()
 {
-  // checkUI();
+  checkUI();
   //digitalWrite(9, LOW);           // release brake DONT USE, IT DOESN'T UNBREAK
 
   //for (int i = 0; i <= 9; i++)      // repeat x number of times
   //{
   lastBreathTime = millis();
-  Serial.print(millis());
   inhale();
-  Serial.print(millis());
   postInhalePause();
   exhale();
   postExhalePause();
@@ -117,6 +98,20 @@ void loop()
   //for (;;)    // for testing purposes
   //{
   //}
+}
+
+
+void printCycleInfo(String cycleStageName, int current)
+{
+  if(millis() % 100 == 0)
+  {
+    Serial.print(cycleStageName);
+    Serial.print("\t\t");
+    Serial.print(millis());
+    Serial.print("\t\t");
+    Serial.print(current);
+    Serial.println();
+  }
 }
 
 // *********** Inhale ***********
@@ -136,11 +131,11 @@ void inhale ()
       }
     }
     setMotor1Speed(mSpeed);
-    Serial.print("forwards , ");      // don't remove these or the current readings will update so quick
-    Serial.println(current);          // thing to do: read current at regular time intervals
-    // checkUI();
+    checkUI();
+    printCycleInfo("inhale",current);
   }
 }
+
 
 // *********** post inhale pause ***********
 void postInhalePause ()
@@ -150,11 +145,11 @@ void postInhalePause ()
   {
     setMotor1Speed(0);
     //digitalWrite(9, HIGH);          // apply brake DON'T USE
-    // checkUI();
-    Serial.print("paused ");
-    Serial.println(current);
+    checkUI();
+    printCycleInfo("piPause",current);
   }
 }
+
 
 // *********** exhale ***********
 void exhale ()
@@ -175,11 +170,11 @@ void exhale ()
       }
     }
     setMotor1Speed(revAccelSpeed);
-    // checkUI();
-    Serial.print("reverse , ");
-    Serial.println(current);
+    checkUI();
+    printCycleInfo("exhale",current);
   }
 }
+
 
 // *********** post exhale pause ***********
 void postExhalePause ()
@@ -188,11 +183,12 @@ void postExhalePause ()
   {
     setMotor1Speed(0);
     //digitalWrite(9, HIGH);          // apply brake DON'T USE
-    // checkUI();
-    Serial.print("paused ");
-    Serial.println(millis() - lastBreathTime);
+    checkUI();
+    printCycleInfo("pePause",current);
   }
 }
+
+
 
 // ****** drive motors ******
 void setMotor1Speed(int M1speed)
@@ -208,68 +204,68 @@ void setMotor1Speed(int M1speed)
   analogWrite(PWM_A, M1speed * 51 / 80); // default to using analogWrite, mapping 400 to 255
 }
 
-// // ***** check buttons & POTs and flash LEDs *****
-// void checkUI()          // check buttons and POTs
-// {
-//   current = getCurrentM1();
-//   a1 = analogRead(A1);
-//   if (analogRead(A2) > maxPressurePOT + 1 || analogRead(A2) < maxPressurePOT - 1) // if values are not equal within a tollerance
-//   {
-//     maxPressurePOT = analogRead(A2);                              // read POT
-//     if (maxPressurePOT > 950 || maxPressurePOT < 50) redLEDonTime = millis();
-//     else POTblueLEDonTime = millis();                                // flash LEDs for feedback
-//     maxPressurePOTconstrianed = constrain(maxPressurePOT, 50, 950);          // constrain POT values to account for tollerance
-//   }
+// ***** check buttons & POTs and flash LEDs *****
+void checkUI()          // check buttons and POTs
+{
+  current = getCurrentM1();
+  a1 = analogRead(A1);
+  if (analogRead(A2) > maxPressurePOT + 1 || analogRead(A2) < maxPressurePOT - 1) // if values are not equal within a tollerance
+  {
+    maxPressurePOT = analogRead(A2);                              // read POT
+    if (maxPressurePOT > 950 || maxPressurePOT < 50) redLEDonTime = millis();
+    else POTblueLEDonTime = millis();                                // flash LEDs for feedback
+    maxPressurePOTconstrianed = constrain(maxPressurePOT, 50, 950);          // constrain POT values to account for tollerance
+  }
 
-//   currentLimit = map(maxPressurePOTconstrianed, 50, 950, 800, 1200);       // set current limit
-//   currentLimit = constrain(currentLimit, 800, 1200);            // constrain current limits
-//   revTimeSetting = map(currentLimit, 800, 1200, 650, 850);      // set reversing time period
-//   revTimeSetting = constrain(revTimeSetting, 650, 850);         // constrain reverse time limits
+  currentLimit = map(maxPressurePOTconstrianed, 50, 950, 800, 1200);       // set current limit
+  currentLimit = constrain(currentLimit, 800, 1200);            // constrain current limits
+  revTimeSetting = map(currentLimit, 800, 1200, 650, 850);      // set reversing time period
+  revTimeSetting = constrain(revTimeSetting, 650, 850);         // constrain reverse time limits
 
-//   // frequency up button reading
-//   int reading2 = digitalRead(freqUpButtonPin);
-//   // debounce
-//   if (reading2 != lastButtonState2) lastDebounceTime2 = millis();    // reset the debouncing timer if state change
-//   if ((millis() - lastDebounceTime2) > debounceDelay) {              // if button pressed for longer than delay
-//     if (reading2 != buttonState2)
-//     {
-//       buttonState2 = reading2;
-//       if (buttonState2 == LOW)
-//       {
-//         freqSettingCounter++;         // add 1 to breath per min setting
-//         if (freqSettingCounter <= 30) blueLEDonTime = millis();
-//         else redLEDonTime = millis();
-//       }
-//     }
-//   }
-//   lastButtonState2 = reading2;
+  // frequency up button reading
+  int reading2 = digitalRead(freqUpButtonPin);
+  // debounce
+  if (reading2 != lastButtonState2) lastDebounceTime2 = millis();    // reset the debouncing timer if state change
+  if ((millis() - lastDebounceTime2) > debounceDelay) {              // if button pressed for longer than delay
+    if (reading2 != buttonState2)
+    {
+      buttonState2 = reading2;
+      if (buttonState2 == LOW)
+      {
+        bpm++;         // add 1 to breath per min setting
+        if (bpm <= 30) blueLEDonTime = millis();
+        else redLEDonTime = millis();
+      }
+    }
+  }
+  lastButtonState2 = reading2;
 
-//   // frequency down button reading
-//   int reading6 = digitalRead(freqDownButtonPin);
-//   // debounce
-//   if (reading6 != lastButtonState6) lastDebounceTime6 = millis();    // reset the debouncing timer if state change
-//   if ((millis() - lastDebounceTime6) > debounceDelay) {              // if button pressed for longer than delay
-//     if (reading6 != buttonState6)
-//     {
-//       buttonState6 = reading6;
-//       if (buttonState6 == LOW)
-//       {
-//         freqSettingCounter--;         // take away 1 from breath per min setting
-//         if (freqSettingCounter >= 10) blueLEDonTime = millis();
-//         else redLEDonTime = millis();
-//       }
-//     }
-//   }
-//   lastButtonState6 = reading6;
+  // frequency down button reading
+  int reading6 = digitalRead(freqDownButtonPin);
+  // debounce
+  if (reading6 != lastButtonState6) lastDebounceTime6 = millis();    // reset the debouncing timer if state change
+  if ((millis() - lastDebounceTime6) > debounceDelay) {              // if button pressed for longer than delay
+    if (reading6 != buttonState6)
+    {
+      buttonState6 = reading6;
+      if (buttonState6 == LOW)
+      {
+        bpm--;         // take away 1 from breath per min setting
+        if (bpm >= 10) blueLEDonTime = millis();
+        else redLEDonTime = millis();
+      }
+    }
+  }
+  lastButtonState6 = reading6;
 
-//   freqSettingCounter = constrain(freqSettingCounter, 10, 30);       // constrain variable to 10-30 b/m
-//   breathPeriod = (60 / freqSettingCounter) * 1000;                  // calc time needed per breath in ms based on UI setting
-//   Serial.println(freqSettingCounter);
+  bpm = constrain(bpm, 10, 30);       // constrain variable to 10-30 b/m
+  breathPeriod = (60 / bpm) * 1000;                  // calc time needed per breath in ms based on UI setting
+  // Serial.println(bpm);
 
-//   if (millis() - blueLEDonTime < 200) digitalWrite(blueLEDPin, HIGH);   // flash blue LED for button press feedback
-//   else digitalWrite(blueLEDPin, LOW);
-//   if (millis() - redLEDonTime < 500) digitalWrite(redLEDPin, HIGH);     // flash red LED if b/m limit reached
-//   else digitalWrite(redLEDPin, LOW);
-//   if (millis() - POTblueLEDonTime < 10) digitalWrite(blueLEDPin, HIGH);   // flash blue LED for button press feedback
-//   else digitalWrite(blueLEDPin, LOW);
-// }
+  if (millis() - blueLEDonTime < 200) digitalWrite(blueLEDPin, HIGH);   // flash blue LED for button press feedback
+  else digitalWrite(blueLEDPin, LOW);
+  if (millis() - redLEDonTime < 500) digitalWrite(redLEDPin, HIGH);     // flash red LED if b/m limit reached
+  else digitalWrite(redLEDPin, LOW);
+  if (millis() - POTblueLEDonTime < 10) digitalWrite(blueLEDPin, HIGH);   // flash blue LED for button press feedback
+  else digitalWrite(blueLEDPin, LOW);
+}
