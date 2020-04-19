ef136a9ac6e3b1482f3aacb5ee460f618504753e
Misha Bruml <mishabruml@gmail.com>
2020-04-19 17:45:06 +0100

    add better motor control timer

--- motor_control_with_flowsensor/motor_control_with_flowsensor.ino
+++ motor_control_with_flowsensor/motor_control_with_flowsensor.ino
@@ -44,7 +44,8 @@ int mSpeed = 0;
 int revTimeSetting = 400;       // time it takes to reverse in ms
 int revSpeed = 400;             // max speed of reverse stroke
 int maxFwdSpeed = -400;         // max speed of motor/acceloration limit
-int currentLimit = 1200;        // current limit
+
+int currentLimit = 500;        // current limit
 
 int postInhaleDwell = 0;
 unsigned long postExhaleDwell = 0;
@@ -59,17 +60,111 @@ void setup()
   // set up motor pins
   pinMode(DIR_A, OUTPUT);
   pinMode(PWM_A, OUTPUT);
-  
+
   Serial.begin(115200);
-  // Serial.println("OpenVent Bristol");
+  Serial.println("OpenVent Bristol");
+  
+  // Motor initialisation
+  while( current < 590 ){
+    setMotor1Speed(-100);
+    current = getCurrentM1();
+  }
+  delay(1000);
+  setMotor1Speed(100);
+  delay(1000);
+  setMotor1Speed(0);
+  delay(5000);
+
+  Timer1.initialize(1000); // 1ms
+  Timer1.attachInterrupt(systemTick); // systemTick to run every 1ms
+  
   // Serial.println("stage\tms\tcurrent\tpressure(pa)\tflow(ml/s)");
+}
+
+volatile int elapsedTime = 0;
+
+volatile int flowReadingMls;
+int flowReadingSum = 0;
+
+const int motorUpdateRate = 10; // update motor every 10 ticks
+unsigned int motorCount = motorUpdateRate;
+volatile int doMotorUpdate = 0;
+
+const int inhaleState = 1;
+const int exhaleState = 0;
+
+volatile int ventState = inhaleState;
+
+const int inhaleDuration = 1000; // ticks
+const int dwell = 100;
+const int exhaleDuration = 800; // ticks
+
+volatile int breathCycle = inhaleDuration;
+
+void systemTick(void)
+{
+  elapsedTime = elapsedTime + 1;
+  flowReadingMls = getFlowRateMls();
+  flowReadingSum = flowReadingSum + flowReadingMls;
+  
+  motorCount = motorCount - 1;
+  if (motorCount == 0)
+  {
+    doMotorUpdate = 1;
+    motorCount = motorUpdateRate;
+    flowReadingMls = flowReadingSum/10; // average over 10 samples
+    flowReadingSum = 0;
+  }
+
+  breathCycle = breathCycle - 1;
+  if (breathCycle == 0)
+  {
+    if (ventState == inhaleState)
+    {
+      ventState = exhaleState;
+      breathCycle = exhaleDuration;
+    }
+    else
+    {
+      ventState = inhaleState;
+      breathCycle = inhaleDuration;
+    }
+  }
 }
 
 void loop()
 {
+  unsigned long elapsedTimeCopy;
+  unsigned int ventStateCopy;
+  int flowReadingCopy;
+
+  if (doMotorUpdate == 1)
+  {
+    noInterrupts();
+    flowReadingCopy = flowReadingMls;
+    doMotorUpdate = 0;
+    ventStateCopy = ventState;
+    elapsedTimeCopy = elapsedTime;
+    interrupts();
+    // Serial.print("Flow ml/s\t");
+    // Serial.println(flowReadingCopy);
+    // Serial.print("\tvent state\t");
+    if(ventStateCopy == inhaleState)
+    {
+      Serial.print("inhale\t");
+      Serial.println(elapsedTimeCopy);
+      setMotor1Speed(-200);
+    }
+    else if(ventStateCopy == exhaleState){
+      Serial.print("exhale\t");
+      Serial.println(elapsedTimeCopy);
+      setMotor1Speed(200);
+    }
+  }
+
   // checkUI();
   // lastBreathTime = millis();
-  inhale();
+  // inhale();
   // postInhalePause();
   // exhale();
   // postExhalePause();
